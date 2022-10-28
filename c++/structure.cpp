c8c648d06dda58998d791762dc628b44ef8a71fe
14shree03 <78526575+14shree03@users.noreply.github.com>
2022-10-28 20:57:56 +0530

    Create structure.cpp

---
+++ c++/structure.cpp
@@ -0,0 +1,13 @@
+// Create a structure variable called myStructure
+struct {
+  int myNum;
+  string myString;
+} myStructure;
+
+// Assign values to members of myStructure
+myStructure.myNum = 1;
+myStructure.myString = "Hello World!";
+
+// Print members of myStructure
+cout << myStructure.myNum << "\n";
+cout << myStructure.myString << "\n";
